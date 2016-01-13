ad59c5b885c06709922eb5a6b32d79e618e7ec51
jotakwak <ykwak323@gmail.com>
2016-01-12 16:26:26 -0800

    Instantiate a new User model if the facebook id does not arleady exist in the db

--- server/db/userSigninController.js
+++ server/db/userSigninController.js
@@ -5,6 +5,16 @@ module.exports = {
 
   addUser : function (req, res, next) {
     console.log('req.body is...', req.body);
+    User.findOne({facebook_id: {req.body.id}}).
+      then(function(result){
+        if(!result) {
+          var newUser = new User({name: req.body.name, id: req.body.id});
+          newUser.save();
+          res.send(201);
+        } else {
+          res.send(200);
+        }
+      })
   }
 
 
