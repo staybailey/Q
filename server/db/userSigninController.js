50a24717b0a221046d7159e557eea74a29c8fd21
henry <hlyford11@gmail.com>
2016-01-12 19:31:27 -0800

    finish backend for user account creation; if FB user is not known, create account and send back 201

--- server/db/userSigninController.js
+++ server/db/userSigninController.js
@@ -1,21 +1,20 @@
 var mongoose = require('mongoose');
-// var User = require('./userModel');
+var User = require('./userModel');
 
 module.exports = {
 
-  // addUser : function (req, res, next) {
-  //   console.log('req.body is...', req.body);
-  //   User.findOne({facebook_id: req.body.id})
-  //     .then(function(result){
-  //       if(!result) {
-  //         var newUser = new User({name: req.body.name, id: req.body.id});
-  //         newUser.save();
-  //         res.send(201);
-  //       } else {
-  //         res.send(200);
-  //       }
-  //     })
-  // }
-
-
+  addUser : function (req, res, next) {    
+    // check if the user is in the db
+    User.findOne({id: req.body.id})
+      .then(function(result){
+        // if not, add her and send back 201
+        if (result === null) {          
+          var newUser = new User({name: req.body.name, id: req.body.id});
+          newUser.save();
+          res.send(201);
+        } else {
+          res.send(200);
+        }
+      });
+  }
 }
